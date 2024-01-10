7ae5a99db3d37b15ee9ff83464442e3674c90f85
sean xiao <Sean_Xiao@epam.com>
2024-01-10 21:44:03 +0800

    init

---
+++ src/main/java/com/sean/RuralCommercialBankCertificateSearch/RuralCommercialBankCertificateSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sean.RuralCommercialBankCertificateSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralCommercialBankCertificateSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralCommercialBankCertificateSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sean/RuralCommercialBankCertificateSearch/RuralCommercialBankCertificateSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.RuralCommercialBankCertificateSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralCommercialBankCertificateSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
